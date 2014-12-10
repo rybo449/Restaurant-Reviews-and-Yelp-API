a1f04acb8fbd6f5b20022eceee0867ee27d26d49
rybo449 <sriniketh.nitk@gmail.com>
2014-12-10 11:13:07 -0500

    added grade column

--- 311_parsing.py
+++ 311_parsing.py
@@ -5,7 +5,7 @@ f = open(sys.argv[1])
 reader = csv.reader(f)
 data = []
 for i in reader:
-	data.append([i[1], i[5], i[7], i[8], i[10], i[12], i[13], i[17]])
+	data.append([i[1], i[5], i[7], i[8], i[10], i[12], i[13], i[14], i[17]])
 f.close()
 f = open(sys.argv[2], 'w')
 writer = csv.writer(f, delimiter = ',')
